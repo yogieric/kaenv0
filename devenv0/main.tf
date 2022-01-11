b5decaae9d5176978b50102eb7b996b70abb1c3d
Eric Brown <ericbrown@khanacademy.org>
2022-01-11 04:39:44 +0000

    tweaks

--- devenv0/main.tf
+++ devenv0/main.tf
@@ -4,12 +4,8 @@ provider "google" {
     region = "us-west1"
 }
 
-resource "random_id" "instance_id" {
-    byte_length = 8
-}
-
 resource "google_compute_instance" "default" {
-    name = "flask-vm-${random_id.instance_id.hex}"
+    name = var.instance_name
     machine_type = "f1-micro"
     zone = "us-west1-a"
 
@@ -29,9 +25,11 @@ resource "google_compute_instance" "default" {
         }
     }
 
-#    metadata = {
+    metadata = {
+        ssh-keys="dev:${var.user_pub_key}\n"
+#        ssh-keys="dev:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCeJkVih2fq7OKwMiD8XFAkF7uA62jZbZX9lKuHwGWz0wz7pqw6wSM/BtMe+Qv/pv/3kQwd0elW34W/3ARYTcChk/yC6VB6zpcnXHCuJagkVcOSQPs9AbOym+/E/SUqKs3RCHVjhh1fJuvfiEgGTb9IEOi2PueletTTwNT1LfqcNWXI3hxpCPqp8uHcU5lxc5K3NNQoV3wFnEddfHCmOys9wbBIx/eNyEG8S2PmT1kTLWM4yXYLyPG+OeOrbSxgOfuebYsm5kVB8mQuDOG34pOhxINlm4IbBFpKt9P/L3ws8IZkJQ8a3suwNBzkgpx4oQmlrQmjrVB6anS7WJVdd0YxBBH2T43pZXeCyE8ysdg7BOBmruNIvARJZE2PM4jqUu3YC9+0AHYe85HH/Xh2LXf0VkK6zMPI3QDo3Ax2cPXk+DucqTTA+oTuhEpICzAhUHSkj3OHsZJZ3ASKE08Nl0gWFd5so5Ei8KPLxoFC9okhS1g8yf2W0CxFpGaKgN/ucfs="
 #        ssh-keys= "ebrown:${file("id_rsa.pub")}"
-#    }
+    }
 }
 
 output "ip" {
---
+++ devenv0/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_name" {
+    description = "Name to give instance"
+    type        = string
+}
+
+variable "user_pub_key" {
+    description = "Public key for user login"
+    type        = string
+}
